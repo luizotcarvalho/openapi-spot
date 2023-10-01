6883b29a8bd8cb5685cfbadae20e65bba7d1b9e1
Luiz Otávio Carvalho <luiz.carvalho@buser.com.br>
2023-10-01 14:20:29 -0300

    add openapi watcher

--- back/pets/apps.py
+++ back/pets/apps.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+
 from django.apps import AppConfig
+from django.utils.autoreload import autoreload_started
+
+
+def watch_api_spec(sender, **kwargs):
+    sender.extra_files.add(Path("../api.yaml"))
 
 
 class PetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pets'
+    default_auto_field = "django.db.models.BigAsdutoField"
+    name = "pets"
+
+    def ready(self):
+        autoreload_started.connect(watch_api_spec)
